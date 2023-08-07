f4285f2337f1599839c4d2f444587adb19db789f
RedSkull_7_7 <icaro.nata147@gmail.com>
2023-08-07 07:42:30 -0300

    setup inicial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { reactive } from 'vue';
+</script>
+
+<template>
+  <div class="container">
+    <header class="p-5 mb-4 bg-light rounded-3">
+      <h1>Calculadora aritmética</h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
